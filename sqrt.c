a4a5834a498497ffd2603e58ecfb7cbbe46513a1
Amit yadav <yamit@iitk.ac.in>
2018-08-05 19:52:29 +0530

    Corrected indentation

--- sqrt.c
+++ sqrt.c
@@ -4,8 +4,8 @@ int main(){
   int a,n;
   scanf("%d",&n);
   while(a!=-1.987654321){
-  printf("%f\n",sqrt(n));
-  scanf("%d",&n);
+    printf("%f\n",sqrt(n));
+    scanf("%d",&n);
   }
   return 0;
 }
